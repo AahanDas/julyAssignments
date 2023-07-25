f7e78c46a383079c7dfc126c497ece6bee9c4038
mrdipti <47773098+AahanDas@users.noreply.github.com>
2023-07-25 20:29:31 +0530

    unique element in an Array file commited

---
+++ NonDuplicateElementInArray.java
@@ -0,0 +1,35 @@
+package week2.weekdayprograms;
+
+import java.util.Arrays;
+
+public class NonDuplicateElementInArray {
+
+	public static void main(String[] args) {
+
+		
+		
+		int[] arr = new int[] {2, 1, 2, 1, 3};
+			Arrays.sort(arr); //1 1 2 2 3
+		
+		int len = arr.length;
+		//System.out.println(len);
+		
+		
+		for(int i=0; i<len; i++) {
+			
+			int counter = 0;
+			for(int j=0; j<len; j++) {
+				
+				if(arr[j] == arr[i]) {
+					counter++;
+				}
+				
+			}
+			if(counter ==1) {
+				
+				System.out.println(arr[i]);
+			}
+		}
+	}
+
+}

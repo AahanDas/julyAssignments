23e350f2bdb16f3fd1bdb197289c8471de41076f
mrdipti <47773098+AahanDas@users.noreply.github.com>
2023-07-25 20:30:18 +0530

    Update NonDuplicateElementInArray.java updated

--- NonDuplicateElementInArray.java
+++ NonDuplicateElementInArray.java
@@ -6,27 +6,24 @@ public class NonDuplicateElementInArray {
 
 	public static void main(String[] args) {
 
-		
-		
-		int[] arr = new int[] {2, 1, 2, 1, 3};
-			Arrays.sort(arr); //1 1 2 2 3
-		
+		int[] arr = new int[] { 2, 1, 2, 1, 3 };
+		Arrays.sort(arr); // 1 1 2 2 3
+
 		int len = arr.length;
-		//System.out.println(len);
-		
-		
-		for(int i=0; i<len; i++) {
-			
+		// System.out.println(len);
+
+		for (int i = 0; i < len; i++) {
+
 			int counter = 0;
-			for(int j=0; j<len; j++) {
-				
-				if(arr[j] == arr[i]) {
+			for (int j = 0; j < len; j++) {
+
+				if (arr[j] == arr[i]) {
 					counter++;
 				}
-				
+
 			}
-			if(counter ==1) {
-				
+			if (counter == 1) {
+
 				System.out.println(arr[i]);
 			}
 		}
